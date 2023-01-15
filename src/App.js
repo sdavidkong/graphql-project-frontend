9d9278e8a24fd385c384991b1f1c8b39ff63afc4
S David Kong <SDAVIDKONG@gmail.com>
2023-01-15 13:34:05 -0800

    added component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from "react";
+import BookList from "./components/BookList";
+
 function App() {
   return (
     <div id="main">
       <h1>Edge & Node Team</h1>
+      <BookList />
     </div>
   );
 }
